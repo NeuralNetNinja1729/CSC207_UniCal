81713c9e5b100bdbdfb16c61a85e54f58de16ced
Ashish Ajin Thomas <Coolioboss7@gmail.com>
2024-11-24 10:34:39 -0500

    Change Calendar Day Use Case Complete + Presenter Template

---
+++ src/main/java/interface_adapter/change_calendar_day/ChangeCalendarDayPresenter.java
@@ -0,0 +1,20 @@
+package interface_adapter.change_calendar_day;
+
+import use_case.change_calendar_day.ChangeCalendarDayOutputBoundary;
+import use_case.change_calendar_day.ChangeCalendarDayOutputData;
+
+/**
+ * The Presenter for the Change Month Calendar Use Case.
+ */
+public class ChangeCalendarDayPresenter implements ChangeCalendarDayOutputBoundary {
+
+    public ChangeCalendarDayPresenter() {
+        // TODO
+    }
+
+    @Override
+    public void prepareSuccessView(ChangeCalendarDayOutputData outputData) {
+        // TODO
+    }
+
+}
---
+++ src/main/java/use_case/change_calendar_day/ChangeCalendarDayInteractor.java
@@ -0,0 +1,45 @@
+package use_case.change_calendar_day;
+
+import data_access.CalendarDataAccessObjectFactory;
+import data_access.GetEventsDataAccessInterface;
+import entity.Calendar;
+import entity.Event;
+import interface_adapter.change_calendar_day.ChangeCalendarDayPresenter;
+
+import java.util.ArrayList;
+
+/**
+ * The Change Day Calendar Interactor.
+ */
+public class ChangeCalendarDayInteractor implements ChangeCalendarDayInputBoundary {
+
+    private final CalendarDataAccessObjectFactory calendarDataAccessObjectFactory;
+    private final ChangeCalendarDayPresenter changeCalendarDayPresenter;
+
+    public ChangeCalendarDayInteractor(CalendarDataAccessObjectFactory calendarDataAccessObjectFactory,
+                                         ChangeCalendarDayPresenter changeCalendarDayPresenter) {
+        this.calendarDataAccessObjectFactory = calendarDataAccessObjectFactory;
+        this.changeCalendarDayPresenter = changeCalendarDayPresenter;
+    }
+
+    @Override
+    public void execute(ChangeCalendarDayInputData inputData) {
+
+        final ArrayList<Event> events = new ArrayList<>();
+
+        final ArrayList<Calendar> calendars = inputData.getCalendarList();
+        for (int i = 0; i < calendars.size(); i++) {
+
+            final GetEventsDataAccessInterface getEventsDataAccessObject =
+                    (GetEventsDataAccessInterface) this.calendarDataAccessObjectFactory
+                            .getCalendarDataAccessObject(calendars.get(i));
+
+            events.addAll(getEventsDataAccessObject.fetchEventsDay(inputData.getDate()));
+        }
+
+        final ChangeCalendarDayOutputData changeCalendarDayOutputData =
+                new ChangeCalendarDayOutputData(calendars, events);
+
+        this.changeCalendarDayPresenter.prepareSuccessView(changeCalendarDayOutputData);
+    }
+}
