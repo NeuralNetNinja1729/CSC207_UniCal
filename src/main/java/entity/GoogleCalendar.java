f2d124eeb25614429de2a7c85ab02ff19a868b44
Ashish Ajin Thomas <Coolioboss7@gmail.com>
2024-11-24 10:25:00 -0500

    Entities Updates to store only required values

--- src/main/java/entity/GoogleCalendar.java
+++ src/main/java/entity/GoogleCalendar.java
@@ -1,51 +1,35 @@
 package entity;
 
-import java.util.List;
-import java.util.ArrayList;
-
+/**
+ * Represents a Google Calendar.
+ */
 public class GoogleCalendar implements Calendar {
-    private String calendarName;
-    private String credentials;
-    private String accountName;
-    private List<Event> events;
 
-    public GoogleCalendar(String calendarName, String credentials, String accountName) {
-        this.calendarName = calendarName;
+    private final String credentials;
+    private final String accountName;
+    private final String calendarName;
+
+    public GoogleCalendar(String credentials, String accountName, String calendarName) {
         this.credentials = credentials;
         this.accountName = accountName;
-        this.events = new ArrayList<>();
+        this.calendarName = calendarName;
     }
 
+    @Override
+    public String getCalendarApiName() {
+        return "GoogleCalendar";
+    }
+
+    @Override
     public String getCalendarName() {
         return calendarName;
-    }
-
-    public void setCalendarName(String calendarName) {
-        this.calendarName = calendarName;
     }
 
     public String getCredentials() {
         return credentials;
     }
 
-    public void setCredentials(String credentials) {
-        this.credentials = credentials;
-    }
-
     public String getAccountName() {
         return accountName;
-    }
-
-    public void setAccountName(String accountName) {
-        this.accountName = accountName;
-    }
-
-    public List<Event> getEvents() {
-        return events;
-    }
-
-    @Override
-    public void addEvent(Event event) {
-        events.add(event);
     }
 }
