c5f21a7ac1bdebf652838056ad96e345b75c0731
AaravT17 <aarav.tandon.004@gmail.com>
2024-11-25 20:05:05 -0500

    completed Presenter for change monthly use case

--- src/main/java/interface_adapter/change_calendar_month/ChangeCalendarMonthPresenter.java
+++ src/main/java/interface_adapter/change_calendar_month/ChangeCalendarMonthPresenter.java
@@ -1,23 +1,55 @@
 package interface_adapter.change_calendar_month;
 
 import entity.Event;
+import interface_adapter.ViewManagerModel;
 import use_case.change_calendar_month.ChangeCalendarMonthOutputBoundary;
 import use_case.change_calendar_month.ChangeCalendarMonthOutputData;
+
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * The Presenter for the Change Month Calendar Use Case.
  */
 public class ChangeCalendarMonthPresenter implements ChangeCalendarMonthOutputBoundary {
 
-    public ChangeCalendarMonthPresenter() {
-        // TODO
+    private final ChangeCalendarMonthViewModel viewModel;
+    private final ViewManagerModel viewManagerModel;
+
+    public ChangeCalendarMonthPresenter(ChangeCalendarMonthViewModel changeCalendarMonthViewModel,
+                                        ViewManagerModel viewManagerModel) {
+        this.viewModel = changeCalendarMonthViewModel;
+        this.viewManagerModel = viewManagerModel;
     }
 
     @Override
     public void prepareSuccessView(ChangeCalendarMonthOutputData outputData) {
-        for (Event e : outputData.getEventList()) {
-            System.out.println(e.getEventName());
-        } // this is temporary, just to check that the use-case backend works
-    }
+        Map<String, String> eventsByDate = new HashMap<>();
 
+        // Format the events
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        for (Event event : outputData.getEventList()) {
+            String date = event.getDate().format(dateFormatter);
+            String eventName = event.getEventName();
+
+            // Append the event to the date
+            eventsByDate.putIfAbsent(date, "");
+            eventsByDate.put(date, eventsByDate.get(date) + eventName + "\n");
+        }
+
+        // Update the view model with formatted data
+        ChangeCalendarMonthState state = this.viewModel.getState();
+        state.setCurrCalendarList(outputData.getCalendarList());
+
+        // Store events as a simplified map for easy UI rendering
+        state.setEventMap(eventsByDate);
+
+        // Notify the view model about the state change
+        this.viewModel.setState(state);
+        this.viewModel.firePropertyChanged();
+
+        this.viewManagerModel.setState(viewModel.getViewName());
+        this.viewManagerModel.firePropertyChanged();
+    }
 }
